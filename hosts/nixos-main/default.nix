259c8c4d659069291fb77b085443ee74d4930803
ER28 <l.guerra-rago@pm.me>
2026-06-03 13:47:41 +0200

    wip

--- hosts/nixos-main/default.nix
+++ hosts/nixos-main/default.nix
@@ -20,6 +20,10 @@
     initialPassword = "admin";
   };
 
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
   # Enable sudo without password (optional but convenient)
   security.sudo.wheelNeedsPassword = false;
 

1eabe066a27c14261e824061c0cfde430b06fe55
ER28 <l.guerra-rago@pm.me>
2026-06-03 14:09:12 +0200

    wip

--- flake.nix
+++ flake.nix
@@ -16,10 +16,16 @@
   outputs = { self, nixpkgs, home-manager, agenix, ... }@inputs:
     let
       system = "x86_64-linux";
-      hostname = "nixos-main";
-      username = "admin";
-    in {
-      nixosConfigurations.${hostname} = nixpkgs.lib.nixosSystem {
+      
+      # Define hosts here for easy expansion
+      hosts = {
+        nixos-main = {
+          username = "admin";
+        };
+        # Example: laptop-work = { username = "workuser"; };
+      };
+
+      mkSystemConfig = hostname: { username, ... }: nixpkgs.lib.nixosSystem {
         inherit system;
         specialArgs = { inherit inputs username; };
 
@@ -31,10 +37,29 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.${username} = import (./home + "/${username}/home.nix");
+            home-manager.users.${username} = import ../home/${username}/home.nix;
           }
         ];
       };
+    in {
+      nixosConfigurations = nixpkgs.lib.genAttrs (builtins.attrNames hosts) (hostname: 
+        mkSystemConfig hostname (hosts.${hostname})
+      );
+
+      # ISO Image builder
+      nixosIso = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./hosts/nixos-main/default.nix
+          agenix.nixosModules.default
+          {
+            console.setup.keyboardConfiguration.layout = "us";
+            services.openssh.enable = true;
+          }
+        ];
+      };
+    };
+
 
       # ISO Image builder
       nixosIso = nixpkgs.lib.nixosSystem {
--- hosts/nixos-main/default.nix
+++ hosts/nixos-main/default.nix
@@ -7,11 +7,13 @@
     ../../modules/common/networking.nix
     ../../modules/common/nix-settings.nix
     ../../modules/common/auto-update.nix
+    ../../modules/common/gui/gnome.nix
   ];
 
   networking.hostName = "nixos-main";
   time.timeZone = "Europe/Paris";           # Change as needed
   i18n.defaultLocale = "en_US.UTF-8";
+  console.keyboardLayout = "azerty";
 
   # User configuration
   users.users.${username} = {
@@ -19,10 +21,6 @@
     extraGroups = [ "wheel" "networkmanager" "audio" "video" ];
     initialPassword = "admin";
   };
-
-  services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
 
   # Enable sudo without password (optional but convenient)
   security.sudo.wheelNeedsPassword = false;

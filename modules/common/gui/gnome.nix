5ee251c03313a84fc662ce8922acc681f6294a68
ER28 <l.guerra-rago@pm.me>
2026-06-03 14:16:08 +0200

    wip

--- modules/common/gui/gnome.nix
+++ modules/common/gui/gnome.nix
@@ -4,5 +4,5 @@
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-  systemd.defaultUnitFilesTargets = [ "graphical.target" ];
+  system.defaultUnits = [ "graphical.target" ];
 }
